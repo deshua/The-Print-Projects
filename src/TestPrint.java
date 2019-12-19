6de6616486986ef36a715f367a385f89756df6b4
mokaPOS <mokapos@mokaPOSs-MacBook-Pro-3.local>
2019-12-19 14:27:38 +0700

    Initial commit

---
+++ src/TestPrint.java
@@ -0,0 +1,9 @@
+//command: git init buat start git sebelum melakukan next command seperti pull dll
+public class TestPrint {
+    public static void main(String[] args) {
+        System.out.println("Hello New Project!");
+    }
+}
+
+//command ls -al to see folder contains
+// command vi .gitignore to ignore some folder yang tidak perlu dimasukin ke repository git
